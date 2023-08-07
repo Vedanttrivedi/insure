e68ea6d04568fdfc4a9a192a9e887d68d7476a0c
Vedanttrivedi <53808109+Vedanttrivedi@users.noreply.github.com>
2023-08-07 11:21:08 +0530

    small printing record bug needs  to be fixed

--- main/java/com/example/insurance/RecordListController.java
+++ main/java/com/example/insurance/RecordListController.java
@@ -18,7 +18,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
-
+//fix small print bug
 public class RecordListController implements Initializable {
 
     @FXML
